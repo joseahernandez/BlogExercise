65b18f392231ee1924fd20ce3d208821414d4074
josehernandz <josehernandz@gmail.com>
2013-12-23 21:20:26 +0100

    Register tag deleter command

--- console.php
+++ console.php
@@ -9,6 +9,7 @@ use joseahernandez\blogExercise\Command\Author\AuthorSearchCommand;
 use joseahernandez\blogExercise\Command\Author\AuthorUpdateCommand;
 use joseahernandez\blogExercise\Command\Author\AuthorDeleteCommand;
 use joseahernandez\blogExercise\Command\Tag\TagCreateCommand;
+use joseahernandez\blogExercise\Command\Tag\TagDeleteCommand;
 
 $console = new Application();
 $console->add(new AuthorCreateCommand($em, new \joseahernandez\blogExercise\Author\AuthorCreator($em)));
@@ -17,5 +18,6 @@ $console->add(new AuthorUpdateCommand($em, new \joseahernandez\blogExercise\Auth
 $console->add(new AuthorDeleteCommand($em));
 
 $console->add(new TagCreateCommand($em, new \joseahernandez\blogExercise\Tag\TagCreator($em)));
+$console->add(new TagDeleteCommand($em, new \joseahernandez\blogExercise\Tag\TagDeleter($em)));
 
 $console->run();
