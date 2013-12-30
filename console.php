ccd4132b54f5510b640c55bfa6cd69d7b20d572f
josehernandz <josehernandz@gmail.com>
2013-12-30 01:44:07 +0100

    Register command to create articles

--- console.php
+++ console.php
@@ -12,6 +12,7 @@ use joseahernandez\blogExercise\Command\Tag\TagCreateCommand;
 use joseahernandez\blogExercise\Command\Tag\TagDeleteCommand;
 use joseahernandez\blogExercise\Command\Tag\TagUpdateCommand;
 use joseahernandez\blogExercise\Command\Tag\TagSearchCommand;
+use joseahernandez\blogExercise\Command\Article\ArticleCreateCommand;
 
 $console = new Application();
 $console->add(new AuthorCreateCommand($em, new \joseahernandez\blogExercise\Author\AuthorCreator($em)));
@@ -23,5 +24,7 @@ $console->add(new TagCreateCommand($em, new \joseahernandez\blogExercise\Tag\Tag
 $console->add(new TagDeleteCommand($em, new \joseahernandez\blogExercise\Tag\TagDeleter($em)));
 $console->add(new TagUpdateCommand($em, new \joseahernandez\blogExercise\Tag\TagModificator($em)));
 $console->add(new TagSearchCommand($em));
+
+$console->add(new ArticleCreateCommand($em, new \joseahernandez\blogExercise\Article\ArticleCreator($em)));
 
 $console->run();
