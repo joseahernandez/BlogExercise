79ee715964a0b9b7b6625b52de824376882d145e
josehernandz <josehernandz@gmail.com>
2014-01-08 00:28:48 +0100

    Register command to modify articles

--- console.php
+++ console.php
@@ -14,6 +14,7 @@ use joseahernandez\blogExercise\Command\Tag\TagUpdateCommand;
 use joseahernandez\blogExercise\Command\Tag\TagSearchCommand;
 use joseahernandez\blogExercise\Command\Article\ArticleCreateCommand;
 use joseahernandez\blogExercise\Command\Article\ArticleDeleteCommand;
+use joseahernandez\blogExercise\Command\Article\ArticleUpdateCommand;
 
 $console = new Application();
 $console->add(new AuthorCreateCommand($em, new \joseahernandez\blogExercise\Author\AuthorCreator($em)));
@@ -28,5 +29,6 @@ $console->add(new TagSearchCommand($em));
 
 $console->add(new ArticleCreateCommand($em, new \joseahernandez\blogExercise\Article\ArticleCreator($em)));
 $console->add(new ArticleDeleteCommand($em, new \joseahernandez\blogExercise\Article\ArticleDeleter($em)));
+$console->add(new ArticleUpdateCommand($em, new \joseahernandez\blogExercise\Article\ArticleModificator($em)));
 
 $console->run();
